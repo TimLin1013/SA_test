e270586031fb0b3c2f89bb989086a50e48977545
LAPTOP-81IJOBPV\user <1125899906842624abc@gmail.com>
2023-12-13 16:38:25 +0800

    update borrowrecordController String轉Timestamp

--- SA_test/WEB-INF/classes/controller/borrowrecordController.java
+++ SA_test/WEB-INF/classes/controller/borrowrecordController.java
@@ -1,5 +1,8 @@
 package controller;
-
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.io.*;
 import java.sql.Timestamp;
 import javax.servlet.*;
@@ -15,18 +18,28 @@ public class borrowrecordController extends HttpServlet{
   private borrowrecordHelper br =  borrowrecordHelper.getHelper();
   public void doPut(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-      JsonReader jsr = new JsonReader(request);
-      JSONObject jso = jsr.getObject();
-      String borrow_time = jso.getString("borrow-time");
-      int instrument_id=jso.getInt("id");
-      int member_id=jso.getInt("instrument_id");
-      Timestamp borrowTime = Timestamp.valueOf(borrow_time);
-      borrowrecord b = new borrowrecord(borrowTime,instrument_id,member_id);
+	   String borrowtime = request.getParameter("borrow-time");
+	   String instrument_id = request.getParameter("instrument_id");
+	   String id = request.getParameter("id");
+	   JSONObject resp = new JSONObject();
+	  //2023-12-13T18:27 時間格式
+	   try {
+           SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+           java.util.Date parsedDate = dateFormat.parse(borrowtime);
+           Timestamp borrow_time = new Timestamp(parsedDate.getTime());
+           resp.put("borrowTime", borrow_time);
+       } catch (ParseException e) {
+           e.printStackTrace();
+       }
       
-      JSONObject data=br.createrecord(b);
-      JSONObject resp = new JSONObject();
-      resp.put("response",data);
-      jsr.response(resp, response);
+      //resp.put("response",data);
+      //resp.put("borrowTime", borrow_time);
+      //resp.put("borrow_time", borrow_time);
+      resp.put("id", id);
+      resp.put("status", "0");
+      response.setContentType("application/json");
+      response.setCharacterEncoding("UTF-8");
+      response.getWriter().write(resp.toString());
       
   }
 }
